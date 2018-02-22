c2b9c965d12ed813786269f5bc9bdb87670cddeb
Tuomas Karna <tuomas.karna@intel.com>
2018-02-21 16:35:13 -0800

    fix compile

--- src/cpu/avx512_common_pooling3D.cpp
+++ src/cpu/avx512_common_pooling3D.cpp
@@ -32,7 +32,7 @@ template <data_type_t data_type, data_type_t acc_type>
 void avx512_common_pooling3D_fwd_t<data_type, acc_type>::execute_forward() {
     using namespace alg_kind;
     using namespace prop_kind;
-    auto alg = conf_.desc()->alg_kind;
+    // auto alg = conf_.desc()->alg_kind;
 
     auto src = reinterpret_cast<const data_t *>(this->input_memory(0));
     auto dst = reinterpret_cast<data_t *>(this->memory(0));
