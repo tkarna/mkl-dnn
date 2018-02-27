9835a8eaf52205cca58f4101c9a6b9e69cd8a940
Tuomas Karna <tuomas.karna@intel.com>
2018-02-27 08:14:24 -0800

    add backward avg pooling to test
    - backward max pooling is not supported yet
    - add cosmoflow/medical imaging pooling layers

--- examples/test_pooling_fwd_ref.cpp
+++ examples/test_pooling_fwd_ref.cpp
@@ -82,26 +82,26 @@ void compute_reference_fwd_pool(algorithm alg,
     auto dst_md = dst_pd.desc();
 
     const int MB = src_md.data.dims[0];
+    const int ID = src_md.data.dims[2];
     const int IH = src_md.data.dims[3];
     const int IW = src_md.data.dims[4];
-    const int ID = src_md.data.dims[2];
 
     const int OC = dst_md.data.dims[1];
     const int OD = dst_md.data.dims[2];
     const int OH = dst_md.data.dims[3];
     const int OW = dst_md.data.dims[4];
 
-    const int KH = kernel[0];
-    const int KW = kernel[1];
-    const int KD = kernel[2];
+    const int KD = kernel[0];
+    const int KH = kernel[1];
+    const int KW = kernel[2];
 
-    const int SH = strides[0];
-    const int SW = strides[1];
-    const int SD = strides[2];
+    const int SD = strides[0];
+    const int SH = strides[1];
+    const int SW = strides[2];
 
-    const int padT = padding[0];
-    const int padL = padding[1];
-    const int padD = padding[2];
+    const int padD = padding[0];
+    const int padT = padding[1];
+    const int padL = padding[2];
 
     auto apply_offset = [=](int index, int offset) {
         return (index > offset) ? index - offset : 0;
@@ -196,13 +196,137 @@ void compute_reference_fwd_pool(algorithm alg,
     }
 }
 
-void compute_fwd_pool(algorithm pooling_alg,
-                      const memory &src_mem,
-                      const memory &dst_mem,
-                      const memory::dims &kernel,
-                      const memory::dims &strides,
-                      const memory::dims &padding
-                      ) {
+void compute_reference_bkw_pool(algorithm alg,
+                                const memory &diff_dst_mem,
+                                const memory &diff_src_mem,
+                                const memory::dims &kernel,
+                                const memory::dims &strides,
+                                const memory::dims &padding) {
+    float *diff_src = (float*)diff_src_mem.get_data_handle();
+    float *diff_dst = (float*)diff_dst_mem.get_data_handle();
+
+    auto diff_src_pd = diff_src_mem.get_primitive_desc();
+    auto diff_dst_pd = diff_dst_mem.get_primitive_desc();
+
+    auto diff_src_md = diff_src_pd.desc();
+    auto diff_dst_md = diff_dst_pd.desc();
+
+    const int MB = diff_src_md.data.dims[0];
+    const int IH = diff_src_md.data.dims[3];
+    const int IW = diff_src_md.data.dims[4];
+    const int ID = diff_src_md.data.dims[2];
+
+    const int OC = diff_dst_md.data.dims[1];
+    const int OD = diff_dst_md.data.dims[2];
+    const int OH = diff_dst_md.data.dims[3];
+    const int OW = diff_dst_md.data.dims[4];
+
+    const int KD = kernel[0];
+    const int KH = kernel[1];
+    const int KW = kernel[2];
+
+    const int SD = strides[0];
+    const int SH = strides[1];
+    const int SW = strides[2];
+
+    const int padD = padding[0];
+    const int padT = padding[1];
+    const int padL = padding[2];
+
+    auto ker_zero = [=](int _mb, int _oc) {
+        for (int id = 0; id < ID; ++id) {
+            for (int ih = 0; ih < IH; ++ih) {
+                for (int iw = 0; iw < IW; ++iw) {
+                    diff_src[ptr_off_f(diff_src_md, _mb, _oc, id, ih, iw)] = float(0);
+                }
+            }
+        }
+    };
+
+    // NOTE omitting max kernel for now
+    // auto ker_max = [=](const data_t *d, int mb, int oc, int od, int oh, int ow) {
+    //     const size_t ws_off = ws_d.off(mb, oc, od, oh, ow);
+    //     const int index = ws_d.data_type() == data_type::u8
+    //         ? (int)ws[ws_off] : ((int *)ws)[ws_off];
+    //     const int kd = index / (KH*KW);
+    //     const int kw = (index % (KH*KW)) % KW;
+    //     const int kh = (index % (KH*KW)) / KW;
+    //     const int id = od * SD - padD + kd;
+    //     const int ih = oh * SH - padT + kh;
+    //     const int iw = ow * SW - padL + kw;
+    //
+    //     diff_src[diff_src_d.off(mb, oc, id, ih, iw)] += d[0];
+    // };
+
+    auto ker_avg = [=](const float *d, int mb, int oc, int od, int oh, int ow) {
+        int id_start = std::max(od*SD - padD, 0);
+        int ih_start = std::max(oh*SH - padT, 0);
+        int iw_start = std::max(ow*SW - padL, 0);
+        int id_end = std::min(od*SD - padD + KD, ID);
+        int ih_end = std::min(oh*SH - padT + KH, IH);
+        int iw_end = std::min(ow*SW - padL + KW, IW);
+
+        // NOTE assuming alg == pooling_avg
+        int num_summands =  (id_end - id_start)*(ih_end - ih_start)*(iw_end - iw_start);
+
+        for (int id = id_start; id < id_end; ++id) {
+            for (int ih = ih_start; ih < ih_end; ++ih) {
+                for (int iw = iw_start; iw < iw_end; ++iw) {
+                    diff_src[ptr_off_f(diff_src_md, mb, oc, id, ih, iw)] += d[0] / num_summands;
+                }
+            }
+        }
+    };
+
+//     if (alg == pooling_max) {
+// #       pragma omp parallel for collapse(2) schedule(static)
+//         for (int mb = 0; mb < MB; ++mb) {
+//             for (int oc = 0; oc < OC; ++oc) {
+//                 ker_zero(mb, oc);
+//                 for (int od = 0; od < OD; ++od) {
+//                     for (int oh = 0; oh < OH; ++oh) {
+//                         for (int ow = 0; ow < OW; ++ow) {
+//                             const float *d =
+//                                 &diff_dst[ptr_off_f(diff_dst_md, mb, oc, od, oh, ow)];
+//                             ker_max(d, mb, oc, od, oh, ow);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+    if (alg == pooling_avg) {
+#       pragma omp parallel for collapse(2) schedule(static)
+        for (int mb = 0; mb < MB; ++mb) {
+            for (int oc = 0; oc < OC; ++oc) {
+                ker_zero(mb, oc);
+                for (int od = 0; od < OD; ++od) {
+                    for (int oh = 0; oh < OH; ++oh) {
+                        for (int ow = 0; ow < OW; ++ow) {
+                            const float *d =
+                                &diff_dst[ptr_off_f(diff_dst_md, mb, oc, od, oh, ow)];
+                            ker_avg(d, mb, oc, od, oh, ow);
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        printf("Reference max pooling has not been implemented\n");
+        exit(-1);
+    }
+}
+
+void compute_pool(std::string direction,
+                  algorithm pooling_alg,
+                  const memory &src_mem,
+                  const memory &dst_mem,
+                  const memory &diff_src_mem,
+                  const memory &diff_dst_mem,
+                  const memory::dims &kernel,
+                  const memory::dims &strides,
+                  const memory::dims &padding
+                  ) {
 
     auto cpu_engine = engine(engine::cpu, 0);
 
@@ -228,33 +352,63 @@ void compute_fwd_pool(algorithm pooling_alg,
     auto ws_mem = with_workspace ? memory(ws_pd) : dst_mem;
 
     /* create forward op primitive */
-    auto pool_op = with_workspace ?
+    auto pool_fwd_op = with_workspace ?
        pooling_forward(pool_fwd_pd, src_mem, dst_mem, ws_mem) :
        pooling_forward(pool_fwd_pd, src_mem, dst_mem);
 
     // create network array
     std::vector<primitive> net;
 
-    net.push_back(pool_op);
+    net.push_back(pool_fwd_op);
+
+    if (direction == "both") {
+        auto diff_dst_pd = diff_dst_mem.get_primitive_desc();
+        auto diff_src_pd = diff_src_mem.get_primitive_desc();
+
+        auto diff_dst_md = diff_dst_pd.desc();
+        auto diff_src_md = diff_src_pd.desc();
+
+        /* op descriptors */
+        auto pool_bkw_desc = pooling_backward::desc(pooling_alg,
+            diff_src_md, diff_dst_md, strides, kernel, padding, padding,
+            padding_kind::zero);
+
+        /* primitive op descriptors */
+        auto pool_bkw_pd = pooling_backward::primitive_desc(pool_bkw_desc,
+                                                            cpu_engine,
+                                                            pool_fwd_pd);
+
+        /* create forward op primitive */
+        auto pool_bkw_op = pooling_backward(pool_bkw_pd, diff_dst_mem, diff_src_mem);
+
+        net.push_back(pool_bkw_op);
+    }
+
     // Execute
     stream(stream::kind::eager).submit(net).wait();
+
 }
 
-bool assert_pooling_3d(algorithm pooling_alg,
+bool assert_pooling_3d(std::string direction, algorithm pooling_alg,
                        const int nbatch, const int channels,
                        const int in_height, const int in_width, const int in_depth,
                        const int ker_height,const int ker_width, const int ker_depth,
                        const int out_height, const int out_width, const int out_depth,
+                       const int stride_height, const int stride_width, const int stride_depth,
+                       const int pad_height, const int pad_width, const int pad_depth,
                        bool fill_with_floats, float tolerance, bool print_arrays = true){
-
     auto cpu_engine = engine(engine::cpu, 0);
 
+    if (direction == "both" && pooling_alg == pooling_max) {
+        std::cout << "Backward operator for pooling max has not been implemented" << std::endl;
+        exit(-1);
+    }
     // Dimensions of memory to be allocated
     memory::dims src_dims = {nbatch, channels, in_depth, in_height, in_width};
     memory::dims dst_dims = {nbatch, channels, out_depth, out_height, out_width};
 
-    auto strides = {1, 1, 1};
-    auto padding = {0, 0, 0};
+    auto strides = {stride_depth, stride_height, stride_width};
+    auto padding = {pad_depth, pad_height, pad_width};
     auto kernel = {ker_depth, ker_height, ker_width};
 
     std::vector<float> vect_src(std::accumulate(src_dims.begin(),
@@ -263,6 +417,12 @@ bool assert_pooling_3d(algorithm pooling_alg,
         dst_dims.end(), 1, std::multiplies<uint32_t>()));
     std::vector<float> vect_ref_dst(std::accumulate(dst_dims.begin(),
         dst_dims.end(), 1, std::multiplies<uint32_t>()));
+    std::vector<float> vect_diff_src(std::accumulate(src_dims.begin(),
+        src_dims.end(), 1, std::multiplies<uint32_t>()));
+    std::vector<float> vect_diff_dst(std::accumulate(dst_dims.begin(),
+        dst_dims.end(), 1, std::multiplies<uint32_t>()));
+    std::vector<float> vect_ref_diff_src(std::accumulate(src_dims.begin(),
+        src_dims.end(), 1, std::multiplies<uint32_t>()));
 
     /* create memory for user data */
     auto src_memory = memory({{{src_dims},
@@ -275,24 +435,36 @@ bool assert_pooling_3d(algorithm pooling_alg,
                                  memory::data_type::f32, memory::format::ncdhw},
                                  cpu_engine}, vect_ref_dst.data());
 
+    auto diff_src_memory = memory({{{src_dims},
+                             memory::data_type::f32, memory::format::ncdhw},
+                             cpu_engine}, vect_diff_src.data());
+    auto diff_dst_memory = memory({{{dst_dims},
+                             memory::data_type::f32, memory::format::ncdhw},
+                             cpu_engine}, vect_diff_dst.data());
+    auto ref_diff_src_memory = memory({{{src_dims},
+                             memory::data_type::f32, memory::format::ncdhw},
+                             cpu_engine}, vect_ref_diff_src.data());
+
     // fill input array with random numbers
     if (fill_with_floats) {
         // floats between 1 and 25
         for (size_t i = 0; i < vect_src.size(); i++)
             vect_src[i] = 1 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(25-1)));
+        for (size_t i = 0; i < vect_diff_dst.size(); i++)
+            vect_diff_dst[i] = 1 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(25-1)));
     } else {
         // ints between 1 and 25
         for (size_t i = 0; i < vect_src.size(); i++)
             vect_src[i] = rand() % 25 + 1.0;
+        for (size_t i = 0; i < vect_diff_dst.size(); i++)
+            vect_diff_dst[i] = rand() % 25 + 1.0;
     }
 
     bool success = true;
 
-    std::cout << "Forward pooling:" << std::endl;
-
     auto alg_str = pooling_alg == pooling_max ? "max" : "avg";
     auto float_str = fill_with_floats ? "float" : "int";
-    printf("Input %dx%dx%d kernel %dx%dx%d %s channels=%d bs=%d (%s)\n",
+    printf("%dx%dx%d k=%dx%dx%d %s ch=%d bs=%d (%s)\n",
            in_height, in_width, in_depth,
            ker_height, ker_width, ker_depth, alg_str,
            channels, nbatch, float_str
@@ -308,51 +480,91 @@ bool assert_pooling_3d(algorithm pooling_alg,
         print_array_3d("Reference output", vect_ref_dst.data(), dst_dims[2], dst_dims[3], dst_dims[4]);
     }
 
-    compute_fwd_pool(pooling_alg, src_memory, dst_memory,
-                     kernel, strides, padding);
+    compute_pool(direction, pooling_alg, src_memory, dst_memory,
+                 diff_src_memory, diff_dst_memory,
+                 kernel, strides, padding);
 
     if (print_arrays) {
         // Print the output matrix
         print_array_3d("Output", vect_dst.data(), dst_dims[2], dst_dims[3], dst_dims[4]);
     }
     // Compute error
-    success = success && check_result("output", vect_dst.data(), vect_ref_dst.data(), vect_ref_dst.size(), tolerance);
+    success = success && check_result("fwd output", vect_dst.data(), vect_ref_dst.data(), vect_ref_dst.size(), tolerance);
 
+    if (direction == "both") {
+        compute_reference_bkw_pool(pooling_alg, diff_dst_memory, ref_diff_src_memory,
+                                   kernel, strides, padding);
+
+        // Print the output matrix
+        if (print_arrays) {
+            print_array_3d("Diff output", vect_diff_dst.data(), dst_dims[2], dst_dims[3], dst_dims[4]);
+            print_array_3d("Reference diff input", vect_ref_diff_src.data(), src_dims[2], src_dims[3], src_dims[4]);
+        }
+
+        if (print_arrays) {
+            // Print the output matrix
+            print_array_3d("Diff input", vect_diff_src.data(), src_dims[2], src_dims[3], src_dims[4]);
+        }
+        // Compute error
+        success = success && check_result("bkw output", vect_diff_src.data(), vect_ref_diff_src.data(), vect_ref_diff_src.size(), tolerance);
+    }
     return success;
-
 }
 
-bool test_pool_simple_3d(algorithm pooling_alg, const int insize) {
+bool test_pool_simple_3d(std::string direction, algorithm pooling_alg, const int insize) {
     const int bs=1;
     const int ic=1;
     const int ih=insize, iw=insize, id=insize;
     const int kh=2, kw=2, kd=2;
     const int oh=ih-kh+1, ow=iw-kw+1, od=id-kd+1;
-    return assert_pooling_3d(pooling_alg, bs, ic, ih, iw, id, kh, kw, kd, oh, ow, od, false, 1e-16);
+//     return assert_pooling_3d(direction, pooling_alg, bs, ic, ih, iw, id, kh, kw, kd, oh, ow, od, false, 1e-16);
+    return assert_pooling_3d(direction, pooling_alg, bs, ic, ih, iw, id,
+                    kh, kw, kd, oh, ow, od,
+                    1, 1, 1, 0, 0, 0, false, 1e-16);
 }
 
-bool test_pool_3d(algorithm pooling_alg, const int insize, const int channels, bool fill_with_floats) {
-    const int bs=1;
-    const int ih=insize, iw=insize, id=insize;
-    const int kh=2, kw=2, kd=2;
-    const int oh=ih-kh+1, ow=iw-kw+1, od=id-kd+1;
+bool test_pool_3d(std::string direction, algorithm pooling_alg,
+                  std::vector<int> insize, const int channels,
+                  std::vector<int> kernel, std::vector<int> strides,
+                  std::vector<int> padding, const int bs=1,
+                  bool fill_with_floats=true
+                 ) {
+    const int ih=insize[0], iw=insize[1], id=insize[2];
+    const int kh=kernel[0], kw=kernel[1], kd=kernel[2];
+    const int sh=strides[0], sw=strides[1], sd=strides[2];
+    const int ph=padding[0], pw=padding[1], pd=padding[2];
+    const int oh=(ih-kh+2*ph)/sh+1, ow=(iw-kw+2*pw)/sw+1, od=(id-kd+2*pd)/sd+1;
     float tol = fill_with_floats ? 1e-5 : 1e-25;
-    return assert_pooling_3d(pooling_alg, bs, channels, ih, iw, id, kh, kw, kd, oh, ow, od, fill_with_floats, tol, false);
+    return assert_pooling_3d(direction, pooling_alg, bs, channels, ih, iw, id,
+                    kh, kw, kd, oh, ow, od,
+                    sh, sw, sd, ph, pw, pd, fill_with_floats, tol, false);
 }
 
 int main(int argc, char **argv) {
     bool success = true;
     try {
-        success = success && test_pool_simple_3d(pooling_max, 4);
-        success = success && test_pool_simple_3d(pooling_avg, 4);
+        test_pool_simple_3d("both", pooling_avg, 4);
+
+        // cosmoflow layers
+        //                         input size      ch  kernel     stride     padding   batch
+        success = success && test_pool_3d("both", pooling_avg, {126, 126, 126}, 16, {2, 2, 2}, {2, 2, 2}, {0, 0, 0}, 1);
+        success = success && test_pool_3d("both", pooling_avg, { 60,  60,  60}, 32, {2, 2, 2}, {2, 2, 2}, {0, 0, 0}, 1);
+
+        // medical imaging layers
+        success = success && test_pool_3d("fwd", pooling_max, {334, 300, 396}, 32, {2, 2, 2}, {2, 2, 2}, {0, 0, 0}, 1);
+        success = success && test_pool_3d("fwd", pooling_max, {163, 146, 194}, 32, {2, 2, 2}, {2, 2, 2}, {0, 0, 0}, 1);
+        success = success && test_pool_3d("both", pooling_avg, { 79,  69,  93}, 32, {2, 3, 3}, {1, 1, 1}, {0, 0, 0}, 1, false);
+        success = success && test_pool_3d("both", pooling_avg, { 79,  69,  93}, 32, {2, 3, 3}, {1, 1, 1}, {0, 0, 0}, 1, true);
+
+        // 32, 64 cubes (additional -- not in the applications)
         std::vector<int> in_sizes = {32, 64};
-        std::vector<int> channels = {1, 8, 16};
+        std::vector<int> kernel_sizes = {2, 3};
         for(std::vector<int>::iterator s = in_sizes.begin(); s != in_sizes.end(); ++s) {
-            for(std::vector<int>::iterator c = channels.begin(); c != channels.end(); ++c) {
-                success = success && test_pool_3d(pooling_max, *s, *c, true);
-                success = success && test_pool_3d(pooling_avg, *s, *c, true);
-                success = success && test_pool_3d(pooling_max, *s, *c, false);
-                success = success && test_pool_3d(pooling_avg, *s, *c, false);
+            for(std::vector<int>::iterator k = kernel_sizes.begin(); k != kernel_sizes.end(); ++k) {
+                success = success && test_pool_3d("fwd", pooling_max, {*s ,*s, *s}, 32, {*k, *k, *k}, {1, 1, 1}, {0, 0, 0}, 1, false);
+                success = success && test_pool_3d("fwd", pooling_max, {*s ,*s, *s}, 32, {*k, *k, *k}, {1, 1, 1}, {0, 0, 0}, 1, true);
+                success = success && test_pool_3d("both", pooling_avg, {*s ,*s, *s}, 32, {*k, *k, *k}, {1, 1, 1}, {0, 0, 0}, 1, false);
+                success = success && test_pool_3d("both", pooling_avg, {*s ,*s, *s}, 32, {*k, *k, *k}, {1, 1, 1}, {0, 0, 0}, 1, true);
             }
         }
         if (success) {
