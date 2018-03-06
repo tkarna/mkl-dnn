07a21342036ecc3dfd1ee31d2c9e25e7a0d55437
Tuomas Karna <tuomas.karna@intel.com>
2018-03-06 15:24:19 -0800

    add ndhwc -> nCdhw16c reorder operator
    Also adds a correctness test for reorders.

--- src/cpu/simple_reorder.hpp
+++ src/cpu/simple_reorder.hpp
@@ -480,6 +480,127 @@ struct simple_reorder_impl<SIMPLE_REORDER_TEMPL_CALL,
     }
 };
 
+
+template <SIMPLE_REORDER_TEMPL_DECL>   // TODO FIXME extend to 3D
+struct simple_reorder_impl<SIMPLE_REORDER_TEMPL_CALL,
+    typename utils::enable_if<
+        fmt_i == ndhwc &&  fmt_o == nCdhw16c
+    >::type>
+{
+    SIMPLE_IS_APPLICABLE(false);
+
+    static status_t execute(const cpu_reorder_pd_t *pd,
+        const data_t<type_i> *input, data_t<type_o> *output) {
+        DECLARE_COMMON_PARAMS();
+
+        const auto &dims = input_d.dims();
+        constexpr int blksize = 16;
+        const auto is = input_d.blocking_desc().strides[0];
+        const auto os = output_d.blocking_desc().strides[0];
+
+        round_mode_t rmode = pd->attr()->round_mode_;
+
+        auto ker = [&](const data_t<type_i> *i, data_t<type_o> *o) {
+            if (order_keep) {
+                if (alpha == 1.0 && beta == 0.0) {
+#                   pragma unroll
+                    for (int C = 0; C < dims[1] / blksize; ++C) {
+#                       pragma omp simd
+                        for (int c = 0; c < blksize; ++c) {
+                            o[C * os[1] + c] = qz_a1b0<data_t<type_i>,
+                                data_t<type_o>>()(i[C * blksize + c], rmode);
+                        }
+                    }
+                } else if (alpha == 1.0) {
+#                   pragma unroll
+                    for (int C = 0; C < dims[1] / blksize; ++C) {
+#                       pragma omp simd
+                        for (int c = 0; c < blksize; ++c) {
+                            o[C * os[1] + c] = qz_a1<data_t<type_i>,
+                                data_t<type_o>>()(i[C * blksize + c],
+                                o[C * os[1] + c], beta, rmode);
+                        }
+                    }
+                } else if (beta == 0.0) {
+#                   pragma unroll
+                    for (int C = 0; C < dims[1] / blksize; ++C) {
+#                       pragma omp simd
+                        for (int c = 0; c < blksize; ++c) {
+                            o[C * os[1] + c] = qz_b0<data_t<type_i>,
+                                data_t<type_o>>()(i[C * blksize + c], alpha, rmode);
+                        }
+                    }
+                } else {
+#                   pragma unroll
+                    for (int C = 0; C < dims[1] / blksize; ++C) {
+#                       pragma omp simd
+                        for (int c = 0; c < blksize; ++c) {
+                            o[C * os[1] + c] = qz<data_t<type_i>,
+                                data_t<type_o>>()(i[C * blksize + c],
+                                o[C * os[1] + c], alpha, beta, rmode);
+                        }
+                    }
+                }
+            } else {
+                if (alpha == 1.0 && beta == 0.0) {
+#                   pragma unroll
+                    for (int C = 0; C < dims[1] / blksize; ++C) {
+#                       pragma omp simd
+                        for (int c = 0; c < blksize; ++c) {
+                            o[C * blksize + c] = qz_a1b0<data_t<type_i>,
+                                data_t<type_o>>()(i[C * is[1] + c], rmode);
+                        }
+                    }
+                } else if (alpha == 1.0) {
+#                   pragma unroll
+                    for (int C = 0; C < dims[1] / blksize; ++C) {
+#                       pragma omp simd
+                        for (int c = 0; c < blksize; ++c) {
+                            o[C * blksize + c] = qz_a1<data_t<type_i>,
+                                data_t<type_o>>()(i[C * is[1] + c],
+                                o[C * blksize + c], beta, rmode);
+                        }
+                    }
+                } else if (beta == 0.0) {
+#                   pragma unroll
+                    for (int C = 0; C < dims[1] / blksize; ++C) {
+#                       pragma omp simd
+                        for (int c = 0; c < blksize; ++c) {
+                            o[C * blksize + c] = qz_b0<data_t<type_i>,
+                                data_t<type_o>>()(i[C * is[1] + c], alpha, rmode);
+                        }
+                    }
+                } else {
+#                   pragma unroll
+                    for (int C = 0; C < dims[1] / blksize; ++C) {
+#                       pragma omp simd
+                        for (int c = 0; c < blksize; ++c) {
+                            o[C * blksize + c] = qz<data_t<type_i>,
+                                data_t<type_o>>()(i[C * is[1] + c],
+                               o[C * blksize + c], alpha, beta, rmode);
+                        }
+                    }
+                }
+            }
+        };
+
+#       pragma omp parallel for collapse(3) schedule(static)
+        for (int n = 0; n < dims[0]; ++n) {
+            for (int d = 0; d < dims[2]; ++d) {
+                for (int h = 0; h < dims[3]; ++h) {
+                    for (int w = 0; w < dims[4]; ++w) {
+                        auto i = &input[input_d.blk_off(n, 0, d, h, w)];
+                        auto o = &output[output_d.blk_off(n, 0, d, h, w)];
+                        ker(i, o);
+                    }
+                }
+            }
+        }
+
+        return success;
+    }
+};
+
 template <SIMPLE_REORDER_TEMPL_DECL>
 struct simple_reorder_impl<SIMPLE_REORDER_TEMPL_CALL,
     typename utils::enable_if<fmt_i == chwn
